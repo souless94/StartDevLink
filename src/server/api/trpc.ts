e8c70970bfc671eb02a7d9b9a4215c2d849f4321
souless94 <clwk94@gmail.com>
2024-06-29 18:06:37 +0800

    update firebase settings

--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -81,16 +81,18 @@ export const createTRPCRouter = t.router;
  */
 export const publicProcedure = t.procedure;
 
-if (!process.env.FIREBASE_SERVICE_ACCOUNT_KEY) {
-  throw new Error('FIREBASE_SERVICE_ACCOUNT_KEY environment variable is not defined.');
+if (!process.env.FIREBASE_PRIVATE_KEY || !process.env.FIREBASE_PRIVATE_KEY || !process.env.FIREBASE_PROJECT_ID ) {
+  throw new Error('FIREBASE_SETTINGS environment variable is not defined.');
 }
-
-const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT_KEY;
 
 // Initialize the app with a custom auth variable, limiting the server's access
 if (getApps().length === 0) {
   initializeApp({
-    credential: cert(serviceAccount)
+    credential: cert({
+      projectId: process.env.FIREBASE_PROJECT_ID, // I get no error here
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL, // I get no error here
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+    })
   });
 }
 export const db = getFirestore();
